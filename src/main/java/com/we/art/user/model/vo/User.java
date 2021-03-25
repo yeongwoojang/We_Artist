d67230d8395365d7d18dce28eec9278148fbc7c6
yeongwoojang <sbtmxhs@gmail.com>
2021-03-25 19:52:15 +0900

    1. 채팅사용하기 위한 의존성 추가. 2.  클라이언트 - 소켓 연결 및 통신 테스트 성공 .3 Lombok 라이브러리 적용

---
+++ src/main/java/com/we/art/user/model/vo/User.java
@@ -0,0 +1,15 @@
+package com.we.art.user.model.vo;
+
+import lombok.Data;
+
+@Data
+public class User {
+	private String userId;
+	private String password;
+	private String email;
+	private String name;
+	private String phone;
+	private String nickName;
+	private int fIdx;
+	private String LoginMethod;
+}
