fa03aa9f13575e4e420ad72909108775fb1d8ced
sunmin <sunmin0406@naver.com>
2021-03-26 17:41:27 +0900

    [0326/김선민] 필요없는 파일삭제 / 갤러리 조금더 이쁘게 변경

--- src/main/webapp/WEB-INF/views/gallery/gallery.jsp
+++ src/main/webapp/WEB-INF/views/gallery/gallery.jsp
@@ -1,20 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ include file="/WEB-INF/views/include/head.jsp"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="EUC-KR">
-<title>갤러리 만들어 브렀어어~~</title>
+<%@ include file="/WEB-INF/views/include/topMenuHead.jsp"%>
+	<section id="hero">
+	    <div class="hero-container middle" data-aos="zoom-in" data-aos-delay="100">
+	    </div>
+	 </section>
+	  <!-- End Hero Section -->
 	<script src="https://threejs.org/build/three.min.js"></script>
 	<script src="/resources/js/gallery/OrbitControls.js"></script>
 	<script src="/resources/js/gallery/GLTFLoader.js"></script>
-	<style type="text/css">
-	</style>
-</head>
-<body>
-	<div class="middle" style="width: 80vw; height: 90vh;">
-	</div>
 	<script type='module' src="/resources/js/gallery/ShowGallery.js"></script>
 </body>
 </html>
--- src/main/webapp/resources/js/gallery/ShowGallery.js
+++ src/main/webapp/resources/js/gallery/ShowGallery.js
@@ -1,117 +1,132 @@
 import {GLTFLoader} from '/resources/js/gallery/GLTFLoader.js';
 
-//3d오브젝트를 렌더링할 HTML Element요소
-let middle = document.querySelector('.middle');
-//scene 객체 생성
-const scene = new THREE.Scene();
+// 기본사용한는 전역변수
+let scene, camera, renderer, controls, stats;
+let container = document.querySelector('.middle');
 
-//camera 세팅 
-//시야각, 종횡비, near, far 
-//near : 해당 값보다 가까이 있는 오브젝트는 랜더링하지 않음 / far : 해당값보다 멀리 있는 오브젝트는 렌더링하지 않음
-const camera = new THREE.PerspectiveCamera( 75, middle.clientWidth / middle.clientHeight, 0.1, 1000 );
-camera.position.z = 2;
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( middle.clientWidth, middle.clientHeight );
-middle.appendChild( renderer.domElement );
+let targetList = []; // 클릭할 객체 -> 액자안에 그림 배열
+let mouse = { x:0, y:0}; // 마우스 클릭시 x,y축을 저장
 
-//컨트롤러 추가
-const controls = new THREE.OrbitControls( camera, renderer.domElement );
-controls.update();
+let init = () => {
+    scene = new THREE.Scene();
 
-document.querySelector('canvas').addEventListener('onclick',()=>{console.log('click')});
+    // 기본 세팅
+    const CLIENT_WIDTH = container.clientWidth
+    const CLIENT_HEIGHT = container.clientHeight;
+    const VIEW_ANGLE = 45;
+    const ASPECT = CLIENT_WIDTH / CLIENT_HEIGHT;
+    const NEAR = 0.1;
+    const FAR = 20000;
 
-//빛 추가
-const color = 'white';
-const intensity = 1;
-const ypos = 7;
+    camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+    camera.position.z = 2;
 
-//액자 객체들을 저장하는 배열
-let targetList = [];
+    // middle에 랜더한다
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(CLIENT_WIDTH,CLIENT_HEIGHT);
+    container.appendChild(renderer.domElement);
 
-const leftLight = new THREE.DirectionalLight(color, intensity);
-leftLight.position.set(5, ypos, 0);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // 위,아래로 시점 변경불가능 설정
+    controls.maxPolarAngle = controls.getPolarAngle();
+    controls.minPolarAngle = controls.getPolarAngle();
 
-const rightLight = new THREE.DirectionalLight(color, intensity);
-rightLight.position.set(-5, ypos, 0);
+    //줌속도 , 회전 속도 설정
+    controls.panSpeed = 0.5;
+    controls.rotateSpeed = 0.2;
+    controls.update();
 
-const frontLight = new THREE.DirectionalLight(color, intensity);
-frontLight.position.set(0, ypos, 5);
+    const color = 'white';
+    const intensity = 0.9;
+    const ypos = 0.1;
 
-const backLight = new THREE.DirectionalLight(color, intensity);
-backLight.position.set(0, ypos, -5);
+    const leftLight = new THREE.DirectionalLight(color, intensity);
+    leftLight.position.set(5, ypos, 0);
 
-function dumpObject(obj, lines = [], isLast = true, prefix = '') {
-	const localPrefix = isLast ? '└─' : '├─';
-	lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
-	const newPrefix = prefix + (isLast ? '  ' : '│ ');
-	const lastNdx = obj.children.length - 1;
-	obj.children.forEach((child, ndx) => {
-	  const isLast = ndx === lastNdx;
-	  dumpObject(child, lines, isLast, newPrefix);
-	});
-	return lines;
+    const rightLight = new THREE.DirectionalLight(color, intensity);
+    rightLight.position.set(-5, ypos, 0);
+
+    const frontLight = new THREE.DirectionalLight(color, intensity);
+    frontLight.position.set(0, ypos, 5);
+
+    const backLight = new THREE.DirectionalLight(color, intensity);
+    backLight.position.set(0, ypos, -5);
+
+    let target;
+    const gltfLoader = new GLTFLoader();
+    const url = './resources/landscape_gallery_by_stoneysteiner/scene.gltf';
+    gltfLoader.load(url, (gltf) => {
+       	const root = gltf.scene;
+        scene.add(root);
+
+        // 오브젝트 객체의 정보를 출력해준다
+        console.dir(root.getObjectByName('Cube003'));
+
+        // Cube003의 children은 액자인지 그림만인지... 하튼 객체 찾아냄
+        target = root.getObjectByName('Cube003');
+        for(let item of target.children){
+            targetList.push(item);
+        }
+        // 상하좌우 조명설치
+        root.add(rightLight);
+	    root.add(leftLight);
+	    root.add(frontLight);
+	    root.add(backLight);
+        root.add(camera);
+    });
+
+    document.addEventListener('click', onDocumentMouseDown,false);
+	window.addEventListener('resize',onWindowResize);
+
 }
 
-let target;
-const gltfLoader = new GLTFLoader();
-const url = './resources/landscape_gallery_by_stoneysteiner/scene.gltf';
-gltfLoader.load(url, (gltf) => {
-    const root = gltf.scene;
-    scene.add(root);
-
-	// 오브젝트 객체의 정보를 출력해준다
-	console.log(dumpObject(root).join('\n'));
-	console.dir(root.getObjectByName('Cube003'));
-
-	// Cube003의 children은 액자인지 그림만인지... 하튼 객체 찾아냄
-	target = root.getObjectByName('Cube003');
-	for(let item of target.children){
-		targetList.push(item);
-	}
-	for(let test2 of target.children){
-	   // test2.rotation.x += 0.1;
-	   // test2.rotation.z += 0.1;
-   }
-	// 일단 상하좌우 조명설치
-	// 추후 지붕에서 보이거나 액자마다 할 예정
-	root.add(rightLight);
-	root.add(leftLight);
-	root.add(frontLight);
-	root.add(backLight);
-	root.add(camera);
-	document.addEventListener('click',onDocumentMouseDown);
-});
-
 let onDocumentMouseDown = (event) => {
-	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
     console.log('Click.' + mouse.x + ' : ' + mouse.y);
 
-    let vector = new THREE.Vector3(mouse.x , mouse.y , 1);
     let ray = new THREE.Raycaster();
     ray.setFromCamera( mouse, camera );
     let intersects = ray.intersectObjects( targetList);
     console.dir(intersects);
     if(intersects.length > 0){
         console.log('Hit @' + toString(intersects[0].point) + '\n'+ intersects[0].object.name);
-        //camera.position.x = intersects[0].point.x;
-        //camera.position.z = intersects[0].point.z;
-        
         
         targetList.forEach((e) => {
+            // 클릭했을때 눌린 객체가 그림인지 아닌지 확인하는 조건문
             if(e.name === intersects[0].object.name){
-                let aa = e;
-                e.geometry.center();
-                console.dir(aa);
+				let test = e.clone();
+				test.name = 'test';
+				test.position.set(0,0,0);
+				e.add(test);
+                console.dir(test);
+                console.dir(e);
 
             }
         })
     }
 }
 
-//화면 랜더링
-function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );	
+// 화면 사이즈에 맞춰서 다시 랜더링 해준다
+let onWindowResize = () => {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
+// 백터 값사용시 x,y,z를 정리해서 출력해줌
+let toString = (v) => {
+     return "[ " + v.x + ", " + v.y + ", " + v.z + " ]";
+}
+
+let animate = () =>{
+    requestAnimationFrame(animate);
+    render();
+}
+
+let render = () => {
+    renderer.render(scene,camera);
+}
+
+init();
 animate();
