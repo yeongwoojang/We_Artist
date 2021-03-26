163d77c27b1eca5a0047fd27bc3eb91090176496
sunmin <sunmin0406@naver.com>
2021-03-26 18:23:35 +0900

    [0326/김선민] fileVO, fileUtil 우리 프로젝트 변수에 맞춰서 변경

--- src/main/java/com/we/art/common/util/file/FileUtil.java
+++ src/main/java/com/we/art/common/util/file/FileUtil.java
@@ -11,54 +11,77 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.we.art.common.code.ConfigCode;
 
-
+/*
+	private String fIdx;
+	private String fOrigin;
+	private String fRename;
+	private String fDate;
+	private String userId;
+	private String fSavePath;
+	private int isDel;  
+*/
 public class FileUtil {
-
-	  
-	   public List<FileVo> fileUpload(List<MultipartFile> files) throws IllegalStateException, IOException{
-	      //파일 메타정보를 가지고 반환될 list
-	      List<FileVo> fileDatas = new ArrayList<>();
-	      //파일이 저장될 경로
-	      String savePath = getSavePath();
-	      if(files.size() >= 1 && files.get(0).getOriginalFilename() != null) {
-	         for(MultipartFile multipartFile : files){
-	            //저장될 파일명
-	            String renameFileName = UUID.randomUUID().toString();
-	            //원본 파일명
-	            String originFileName = multipartFile.getOriginalFilename();
-	            
-	            FileVo fileVo = new FileVo();
-	            fileVo.setOriginFileName(originFileName);
-	            fileVo.setRenameFileName(renameFileName);
-	            fileVo.setSavePath(savePath);
-	            fileDatas.add(fileVo);
-	            
-	            saveFile(multipartFile, fileVo);
-	         }
-	      }
-	      return fileDatas;
-	   }
-	   
-	   private String getSavePath() {
-	      Calendar cal = Calendar.getInstance();      
-	      return cal.get(Calendar.YEAR) 
-	            + "/" + (cal.get(Calendar.MONTH) + 1)
-	            + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/";
-	   }
-	   
-	   
-	   private void saveFile(MultipartFile multipartFile, FileVo fileVo) throws IllegalStateException, IOException {
-	      File file = new File(fileVo.getFullPath()+fileVo.getRenameFileName());
-	      if(!file.exists()) {
-	         new File(fileVo.getFullPath()).mkdirs();
-	      }
-	      
-	      multipartFile.transferTo(file);
-	   }
-	   
-	   public void deleteFile(String path, String renameFileName) {
-	      File file = new File(ConfigCode.UPLOAD_PATH + path + renameFileName);
-	      file.delete();
-	   }
-	   
+	
+	public List<FIleVo> fileUpload(List<MultipartFile> files) throws IOException, Exception{
+		
+		List<FIleVo> fileDatas = new ArrayList<FIleVo>();
+		String fSavePath = getSavePath();
+		if(files.size() >= 1 && files.get(0).getOriginalFilename() != null) {
+			for(MultipartFile multipartFile : files) {
+				String fRename = UUID.randomUUID().toString();
+				String fOrigin = multipartFile.getOriginalFilename();
+				
+				FIleVo fileVo = new FIleVo();
+				fileVo.setFOrigin(fOrigin);
+				fileVo.setFRename(fRename);
+				fileVo.setFSavePath(fSavePath);
+				fileDatas.add(fileVo);
+				
+				saveFile(multipartFile,fileVo);
+			}
+		}
+		return fileDatas;
+	}
+	
+	private String getSavePath() {
+		Calendar cal = Calendar.getInstance();
+		return cal.get(Calendar.YEAR)
+				+"/"+ (cal.get(Calendar.MONTH)+1)
+				+"/"+ cal.get(Calendar.DAY_OF_MONTH) + "/";
+	}
+	
+	private void saveFile(MultipartFile multipartFile, FIleVo fileVo) throws Exception, IOException {
+		File file = new File(fileVo.getFullPath() + fileVo.getFRename());
+		if(!file.exists()) {
+			new File(fileVo.getFullPath()).mkdirs();
+		}
+		multipartFile.transferTo(file);
+	}
+	
+	private void deleteFile(String path, String fRename) {
+		File file = new File(ConfigCode.UPLOAD_PATH + path + fRename);
+		file.delete();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/we/art/common/util/file/FileVo.java
+++ src/main/java/com/we/art/common/util/file/FileVo.java
@@ -1,93 +1,22 @@
 package com.we.art.common.util.file;
 
-import java.sql.Date;
-
 import com.we.art.common.code.ConfigCode;
 
+import lombok.Data;
 
-public class FileVo {
-	private int fIdx;
-	private String typeIdx;
-	private String orignFileName;
-	private String renameFileName;
-	private String savePath;
-	private Date regDate;
+@Data
+public class FIleVo {
+	
+	private String fIdx;
+	private String fOrigin;
+	private String fRename;
+	private String fDate;
+	private String userId;
+	private String fSavePath;
 	private int isDel;
 	
-	public int getfIdx() {
-		return fIdx;
-	}
-	
-	public void setfIdx(int fIdx) {
-		this.fIdx = fIdx;
-	}
-	
-	public String getTypeIdx() {
-		return typeIdx;
-	}
-	
-	public void setTypeIdx(String typeIdx) {
-		this.typeIdx = typeIdx;
-	}
-	
-	public String getOriginFileName() {
-		return orignFileName;
-	}
-	
-	public void setOriginFileName(String orginFileName) {
-		this.orignFileName = orginFileName;
-	}
-	
-	public String getRenameFileName() {
-		return renameFileName;
-	}
-	
-	public void setRenameFileName(String renameFileName) {
-		this.renameFileName = renameFileName;
-	}
-	
-	public String getSavePath() {
-		return savePath;
-	}
-	
-	public void setSavePath(String savePath) {
-		this.savePath = savePath;
-	}
-	
-	public Date getRegDate() {
-		return regDate;
-	}
-	
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-	
-	public int getIsDel() {
-		return isDel;
-	}
-	
-	public void setIsDel(int isDel) {
-		this.isDel = isDel;
-	}
 	public String getFullPath() {
-		return ConfigCode.UPLOAD_PATH+ savePath;
+		return ConfigCode.UPLOAD_PATH + fSavePath;
 	}
-	@Override
-	public String toString() {
-		return "FileVO [fIdx=" + fIdx + ", typeIdx=" + typeIdx + ", orginFileName=" + orignFileName
-				+ ", renameFileName=" + renameFileName + ", savePath=" + savePath + ", regDate=" + regDate + ", isDel="
-				+ isDel + "]";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 }
