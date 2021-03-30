60442cf7e061c397dd3482ac4a776a31eefd3d6b
masolfree <masolfree@naver.com>
2021-03-30 09:16:43 +0900

    Revert "[03/30]join.jsp"
    This reverts commit 2c2994462a175d50a112deba61f20cde5ca699a7.

--- src/main/java/com/we/art/user/controller/UserController.java
+++ src/main/java/com/we/art/user/controller/UserController.java
@@ -1,14 +1,8 @@
 package com.we.art.user.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-	
-	@GetMapping("join")
-	public String ShowGallery() {
-		return "user/join";
-	}
 
 }
