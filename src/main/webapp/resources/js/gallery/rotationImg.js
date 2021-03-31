b8aeb3d4ff6569847f94d2f6e20f1db2b7fd682d
sunmin <sunmin0406@naver.com>
2021-03-31 20:27:48 +0900

    input태그 없이 이미지 전송 엉엉ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- src/main/webapp/resources/js/gallery/rotationImg.js
+++ src/main/webapp/resources/js/gallery/rotationImg.js
@@ -19,21 +19,44 @@ upload.addEventListener('change',(e)=>{
 	//sendImg(limg);
 });*/
 
+let fileList = [];
+
 let srcToBlob = (src,outputBox,viewBox) => {
 	let inputObj = src;
 	let outputObj = document.querySelector(outputBox);
 	
 	loadImage(inputObj,(img)=>{
+		console.dir(img);
 		img.toBlob((blob)=>{
-			let rotateFile = new File([blob],'나의이름은',{type : 'JPEG'});
+			let rotateFile = new File([blob],'gellary',{type : 'JPEG'});
+			console.dir(rotateFile);
 			let reader = new FileReader();
 			reader.onload = (e) => {
+				fileList.push(rotateFile);
 				outputObj.src = e.target.result;
 				document.querySelector(viewBox).className = 'd-flex position-absolute';
+				console.dir(fileList);
             }
             reader.readAsDataURL(rotateFile);
 		},'JPEG')		
 	},{orientation : 4});
 }
 
+let sendImageTest = () => {
+	
+	let formData = new FormData();
+	for(let i=0; i<fileList.length; i++){
+		console.dir(fileList[i]);
+		formData.append('test',fileList[i]);
+	}
+	
+	console.dir(formData);
+	fetch('/sendimage',{
+		method : 'POST',
+		body : formData
+	})
+	.then(res => res.text)
+	.then(res => {alert(res)});
+	
+}
 
