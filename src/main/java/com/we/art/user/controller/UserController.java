6b25a39443efdf61daa9099924c0a71a53bf7e85
salirKim <yeoniikim@naver.com>
2021-04-09 09:17:18 +0900

    유저 컨트롤러 주석 수정(무시하셔도돼요)

--- src/main/java/com/we/art/user/controller/UserController.java
+++ src/main/java/com/we/art/user/controller/UserController.java
@@ -122,18 +122,18 @@ public class UserController {
 	public String loginImpl(@ModelAttribute User user, HttpSession session,Model model) {
 		System.out.println(user);
 		String result = null;
-		User userInfo = userService.selectUserForLogin(user); //입력받은 값으 DB를 조회하여 결과를 userInfo에 저
+		User userInfo = userService.selectUserForLogin(user); //입력받은 값으 DB를 조회하여 결과를 userInfo에 저장
 		if (userInfo != null) { //만약에 userInfo가 null이 아니면 (DB에 입력받은 Id가 다면)
-			session.setAttribute("userInfo", userInfo); //세션에 그 유저 아이디를 저
+			session.setAttribute("userInfo", userInfo); //세션에 그 유저 아이디를 저장
 			model.addAttribute("url",ConfigCode.DOMAIN+"/search/main");	//이동할 경로를 설정 
 		}
 		
 		else { //만약에 userInfo가 null이라  (DB에 입력받은 Id가 없다면)
-			model.addAttribute("alertMsg" , "로그인 실패하였습니다.");//result.jsp에 alert안에 들어갈 문자열 지
+			model.addAttribute("alertMsg" , "로그인 실패하였습니다.");//result.jsp에 alert안에 들어갈 문자열 지장
 			model.addAttribute("url",ConfigCode.DOMAIN+"/user/login"); //이동할 경로를 설정 
 		}
 		
-		return "common/result"; //model에 담긴 값을 result.jsp로 전
+		return "common/result"; //model에 담긴 값을 result.jsp로 전달
 
 	}
 
