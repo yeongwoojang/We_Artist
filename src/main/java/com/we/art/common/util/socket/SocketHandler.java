fcf35bfecf9bdf2375aa376c36b68c8f447d0fe0
sunmin <sunmin0406@naver.com>
2021-04-22 09:40:31 +0900

    1

--- src/main/java/com/we/art/common/util/socket/SocketHandler.java
+++ src/main/java/com/we/art/common/util/socket/SocketHandler.java
@@ -11,20 +11,21 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class SocketHandler extends TextWebSocketHandler{
 	
+	// 연결된 유저들을 저장하는 배열
 	List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 	
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		for (WebSocketSession webSocketSession : sessions) {
-            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
-                webSocketSession.sendMessage(message);
+		for (WebSocketSession webSocketSession : sessions) { // 메세지가 온다면
+            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) { // 세션이열려있고, 보낸사람이 아니라면
+                webSocketSession.sendMessage(message); // 메세지 전송
             }
         }
 	}
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		sessions.add(session);
+		sessions.add(session); // 수신된 세션을 세션 목록에 저장
 	}
 	
 }
